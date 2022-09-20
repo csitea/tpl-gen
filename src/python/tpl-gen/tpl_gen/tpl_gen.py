1ba98b36838848bffe8da68d0fd7ed3e22c45a9a
Renato Guimarães <renato.guimaraes@csitea.net>
2022-09-20 19:33:16 -0300

    rfcr: remove SRC variable, replace for TGT

--- src/python/tpl-gen/tpl_gen/tpl_gen.py
+++ src/python/tpl-gen/tpl_gen/tpl_gen.py
@@ -10,8 +10,8 @@ from colorama import Fore, Style
 
 
 def main():
-    ORG_, ENV_, APP_, cnf, src_proj_dir, tgt_proj_dir = set_vars()
-    do_generate(ORG_, ENV_ , APP_ , cnf, src_proj_dir, tgt_proj_dir)
+    ORG_, ENV_, APP_, cnf, tgt_proj_dir = set_vars()
+    do_generate(ORG_, ENV_ , APP_ , cnf, tgt_proj_dir)
 
 def print_warn(msg):
     print(f"{Fore.YELLOW}{msg}{Style.RESET_ALL}")
@@ -50,48 +50,50 @@ def set_vars():
         ENV_ = os.getenv("ENV")
         ORG_ = os.getenv("ORG")
         APP_ = os.getenv("APP")
+        TGT_        = os.getenv("TGT")  # where we get tpl files
+        TPL_CONFIG_ = os.getenv("TPL_CONFIG")  # where we get the config file
 
         product_dir = os.path.join(__file__, "..", "..", "..", "..", "..")
         product_dir = os.path.abspath(product_dir)
         
-        # the tpl-gen and the target project MUST be in the same directory !!!
-        SRC_ = os.getenv("SRC")
-        TGT_ = os.getenv("TGT")
-        
-        if SRC_ == "" or SRC_ is None: 
-            src_proj_dir = os.path.join("/var", "infra")
+        if TGT_ == "" or TGT_ is None: 
+            tgt_proj_dir = os.path.join("/var", "infra")
         else:
-            src_proj_dir = os.path.join("/var", SRC_)
+            tgt_proj_dir = os.path.join("/var", f"{TGT_}")
 
-        json_cnf_file = f"{src_proj_dir}/cnf/env/{ORG_}/{APP_}/{ENV_}.env.json"
+        if TPL_CONFIG_ == "" or TPL_CONFIG_ is None: 
+            tpl_config = os.path.join("/var", "infra", "cnf", "env", f"{ORG_}", f"{APP_}")
+        else:
+            tpl_config = os.path.join("/var", f"{TPL_CONFIG_}")
+
+        json_cnf_file = os.path.join(tpl_config, f"{ENV_}.env.json")
         yaml_cnf_file = json_cnf_file.replace('json', 'yaml')
-
-        print(f"tpl_gen.py ::: using config json file: {json_cnf_file}")
-        time.sleep(1)
 
         # If YAML exists, dump it into JSON and use it.
         if os.path.exists(yaml_cnf_file):
+            print(f"tpl_gen.py ::: using config json file: {yaml_cnf_file}")
             with open(yaml_cnf_file, encoding="utf-8") as file:
                 cnf = yaml.load(file, Loader=yaml.Loader)
 
+            # backwards compatible and also useful for jq
             with open(json_cnf_file, 'w') as file:
                 json.dump(cnf, file, indent=4)
         else:  # otherwise use json
+            print(f"tpl_gen.py ::: using config json file: {json_cnf_file}")
             with open(json_cnf_file, encoding="utf-8") as file:
                 cnf = json.load(file)
-        src_proj_dir = [os.path.join(src_proj_dir, "src", "tpl", "cnf")]
         
         #pprintjson(cnf)
 
     except IndexError as error:
         raise Exception("ERROR in set_vars: ", str(error)) from error
 
-    return ORG_, ENV_, APP_, cnf, src_proj_dir, TGT_
+    return ORG_, ENV_, APP_, cnf, tgt_proj_dir
 
 
-def do_generate(ORG_, ENV_, APP_, cnf, src_proj_dir, tgt_proj_dir):
+def do_generate(ORG_, ENV_, APP_, cnf, tgt_proj_dir):
 
-    for pathname in src_proj_dir:
+    for pathname in [os.path.join(tgt_proj_dir, "src", "tpl")]:  # directory this structure is enforced
         for subdir, _dirs, files in os.walk(pathname):
             for file in files:
                 current_file_path = os.path.join(subdir, file)
@@ -99,6 +101,7 @@ def do_generate(ORG_, ENV_, APP_, cnf, src_proj_dir, tgt_proj_dir):
                     try:
                         print (f"START ::: working on tpl file: {current_file_path}")
                         with open(current_file_path, "r", encoding="utf-8") as current_file:
+                            print(current_file_path)
                             str_tpl = current_file.read()
                             obj_tpl = Environment(loader=BaseLoader) \
                                 .from_string(str_tpl)
@@ -106,15 +109,11 @@ def do_generate(ORG_, ENV_, APP_, cnf, src_proj_dir, tgt_proj_dir):
                             args.update(cnf["env"])
                             rendered = obj_tpl.render(args)
 
-                            current_file = current_file_path.split('/')[-1].replace(".tpl", "")  # name of the file
-                            if tgt_proj_dir == "" or tgt_proj_dir is None:
-                                tgt_file_path = current_file_path.replace("/src/tpl", "", 1) \
-                                    .replace(".tpl", "") \
-                                    .replace(r"%env%", ENV_) \
-                                    .replace(r"%org%", ORG_) \
-                                    .replace(r"%app%", APP_)
-                            else:
-                                tgt_file_path = os.path.join(os.path.join("/var", tgt_proj_dir), current_file)
+                            tgt_file_path = current_file_path.replace("/src/tpl", "", 1) \
+                                .replace(".tpl", "") \
+                                .replace(r"%env%", ENV_) \
+                                .replace(r"%org%", ORG_) \
+                                .replace(r"%app%", APP_)
 
                             if not os.path.exists(os.path.dirname(tgt_file_path)):
                                 os.makedirs(os.path.dirname(tgt_file_path))
