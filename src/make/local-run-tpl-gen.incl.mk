97de096407a4cff72f182c5fc01a506204f339a5
Yordan Georgiev <yordan.georgiev@csitea.net>
2023-06-06 13:32:28 +0300

    230606133043 fix: add the TPL_SRC to the make call

--- src/make/local-run-tpl-gen.incl.mk
+++ src/make/local-run-tpl-gen.incl.mk
@@ -7,7 +7,7 @@ tpl-gen: demand_var-ENV demand_var-ORG demand_var-APP
 
 .PHONY: do-tpl-gen ## @-> apply the environment cnf file into the templates on the tpl-gen container
 do-tpl-gen: demand_var-ORG demand_var-ENV demand_var-APP
-	docker exec -e ORG=$(ORG) -e APP=$(APP) -e ENV=$(ENV) -e TGT=$(TGT) -e SRC=$(SRC) $(ORG)-${PRODUCT}-tpl-gen-con make ${PRODUCT}
+	docker exec -e TPL_SRC=$(TPL_SRC) -e CNF_SRC=$(CNF_SRC) -e ORG=$(ORG) -e APP=$(APP) -e ENV=$(ENV) -e TGT=$(TGT) -e SRC=$(SRC) $(ORG)-${PRODUCT}-tpl-gen-con make ${PRODUCT}
 
 .PHONY: run-tpl-gen ## @-> starts container, renders tpl-gen and destroyes container
 run-tpl-gen: demand_var-ORG demand_var-ENV demand_var-APP
